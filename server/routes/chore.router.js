796aa0e0bef93d48bde029cccd824136ec74a027
Andrew Carey <andrew.r.carey@gmail.com>
2023-01-02 10:43:17 -0600

    GET added in chore router

--- server/routes/chore.router.js
+++ server/routes/chore.router.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
+// returns all chores per family 
+router.get('/', rejectUnauthenticated, (req, res) => {
+  const queryText = `
+  SELECT FROM chore
+  WHERE "family_id" = ($1);
+  `;
+  const queryValues = [req.user.family_id]
+  pool
+    .query(queryText, queryValues)
+    .then(results => res.send(results.rows))
+    .catch(err => {
+      console.log('Error making get request for chores', err);
+      res.sendStatus(500);
+    })
 });
 
-/**
- * POST route template
- */
+
 router.post('/', (req, res) => {
   // POST route code here
 });
